e0fca03853e113f698fec9c84264ac51fc0162eb
Elliana May <me@mause.me>
2022-06-14 22:41:01 +0800

    chore: add missing type annotation

--- duckdb_engine/__init__.py
+++ duckdb_engine/__init__.py
@@ -186,7 +186,7 @@ class Dialect(postgres_dialect):
         return cls
 
     def get_view_names(
-        self, connection: ConnectionWrapper, schema=None, **kwargs: Any
+        self, connection: ConnectionWrapper, schema: str=None, **kwargs: Any
     ) -> List[str]:
         s = "SELECT name FROM sqlite_master WHERE type='view' ORDER BY name"
         rs = connection.exec_driver_sql(s)
