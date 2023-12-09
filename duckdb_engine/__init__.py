fdcedaa97d357976f6f8b71106d0bf8e6e6057f6
Elliana May <me@mause.me>
2023-12-09 12:02:26 +0800

    chore(main): release 0.9.4

--- duckdb_engine/__init__.py
+++ duckdb_engine/__init__.py
@@ -31,7 +31,7 @@ from sqlalchemy.ext.compiler import compiles
 from .config import apply_config, get_core_config
 from .datatypes import ISCHEMA_NAMES, register_extension_types
 
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 sqlalchemy_version = Version(sqlalchemy.__version__)
 duckdb_version: str = duckdb.__version__  # type: ignore[attr-defined]
 supports_attach: bool = Version(duckdb_version) >= Version("0.7.0")
