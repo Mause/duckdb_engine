0c5070698152d4cec13a06e758e4077c51ab2e77
Elliana May <me@mause.me>
2024-01-18 13:38:22 +0800

    add basic class-based parquet query

--- duckdb_engine/tests/snapshots/snap_test_reflection.py
+++ duckdb_engine/tests/snapshots/snap_test_reflection.py
@@ -4,6 +4,17 @@ from snapshottest import GenericRepr, Snapshot
 
 snapshots = Snapshot()
 
+snapshots[
+    "test_parquet_with_declarative_base 1"
+] = """SELECT simple."itemType", simple."stateId", simple."stateMeta", simple.style, simple.description
+FROM "/home/me/duckdb_engine/duckdb_engine/tests/simple.parquet" AS simple"""
+
+snapshots["test_parquet_with_declarative_base 2"] = [
+    GenericRepr(
+        "(41, {'idNum': 0, 'sessionNum': 0, 'sessionName': 'Parquet2021-11-04', 'instanceName': 'ParquetTest', 'smName': 'ParquetTestEngine'}, {'creationTime': datetime.datetime(2021, 11, 5, 0, 34, 49, 937000, tzinfo=<DstTzInfo 'Australia/Perth' AWST+8:00:00 STD>), 'updateTime': datetime.datetime(2021, 11, 5, 0, 34, 49, 937000, tzinfo=<DstTzInfo 'Australia/Perth' AWST+8:00:00 STD>), 'version': 0, 'deleted': False, 'completed': False}, 'style', 'desc')"
+    )
+]
+
 snapshots["test_reflection 1"] = [
     GenericRepr(
         "({'idNum': 0, 'sessionNum': 0, 'sessionName': 'Parquet2021-11-04', 'instanceName': 'ParquetTest', 'smName': 'ParquetTestEngine'}, {'creationTime': datetime.datetime(2021, 11, 5, 0, 34, 49, 937000, tzinfo=<DstTzInfo 'Australia/Perth' AWST+8:00:00 STD>), 'updateTime': datetime.datetime(2021, 11, 5, 0, 34, 49, 937000, tzinfo=<DstTzInfo 'Australia/Perth' AWST+8:00:00 STD>), 'version': 0, 'deleted': False, 'completed': False}, 'style', 41, 'desc')"
--- duckdb_engine/tests/test_reflection.py
+++ duckdb_engine/tests/test_reflection.py
@@ -1,10 +1,25 @@
 from pathlib import Path
+from typing import cast
 
 from snapshottest.module import SnapshotTest
-from sqlalchemy import select, table
+from sqlalchemy import Column, Integer, String, alias, select, table
 from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import FromClause
 
+from duckdb_engine.datatypes import Struct
+
+Base = declarative_base()
 path = Path(__file__).parent / "simple.parquet"
+
+
+class Simple(Base):
+    __tablename__ = str(path)
+    itemType = Column(Integer, primary_key=True)
+    stateId = Column(Struct)
+    stateMeta = Column(Struct)
+    style = Column(String)
+    description = Column(String)
 
 
 def test_reflection(engine: Engine, snapshot: SnapshotTest) -> None:
@@ -14,4 +29,13 @@ def test_reflection(engine: Engine, snapshot: SnapshotTest) -> None:
 
     with engine.connect() as conn:
         res = conn.execute(users).fetchall()
+        snapshot.assert_match(res)
+
+
+def test_parquet_with_declarative_base(engine: Engine, snapshot: SnapshotTest) -> None:
+    query = select(alias(cast(FromClause, Simple), "simple"))
+    snapshot.assert_match(str(query.compile(bind=engine)))
+
+    with engine.connect() as conn:
+        res = conn.execute(query).fetchall()
         snapshot.assert_match(res)
