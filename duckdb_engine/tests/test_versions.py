9c4413e2b04597b2fadb7f61ded2727fc6770434
Elliana May <me@mause.me>
2024-02-14 15:46:05 +0800

    test: add version sanity test

---
+++ duckdb_engine/tests/test_versions.py
@@ -0,0 +1,17 @@
+import os
+
+from importlib_metadata import version
+from pytest import mark
+
+tox_env_name = os.environ.get("TOX_ENV_NAME")
+
+
+@mark.skipif(not tox_env_name, reason="not running inside tox")
+def test_versions() -> None:
+    assert tox_env_name
+    parts = version("duckdb").replace(".", "")
+    assert (
+        tox_env_name == "duckdb_master"
+        or "mypy" in tox_env_name
+        or f"duckdb{parts}" in tox_env_name
+    )
