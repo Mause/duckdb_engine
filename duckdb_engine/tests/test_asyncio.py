4b40ec186d3baf8824e317cf79c12dad8f34570e
Elliana May <me@mause.me>
2024-02-14 13:49:13 +0800

    test: extend to test with models

--- duckdb_engine/tests/test_asyncio.py
+++ duckdb_engine/tests/test_asyncio.py
@@ -1,17 +1,54 @@
-from sqlalchemy.ext.asyncio import (
-    AsyncAttrs,
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
+from datetime import datetime
+
+from pytest import mark
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    Sequence,
+    String,
+    func,
+    select,
 )
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+Base = declarative_base()
 
 
-def test_asyncio():
+class A(Base):
+    __tablename__ = "a"
+
+    id: int = Column(Integer, Sequence("id_seq"), primary_key=True)
+    data: str = Column(String)
+    create_date: datetime = Column(DateTime, server_default=func.now())
+    bs: "B" = relationship("B")
+
+    # required in order to access columns with server defaults
+    # or SQL expression defaults, after a flush, without
+    # triggering an expired load
+    __mapper_args__ = {"eager_defaults": True}
+
+
+class B(Base):
+    __tablename__ = "b"
+    id: int = Column(Integer, Sequence("b_seq"), primary_key=True)
+    a_id: int = Column(ForeignKey("a.id"))
+    data = Column(String)
+
+
+@mark.asyncio
+async def test_asyncio() -> None:
     engine = create_async_engine("duckdb:///:memory:")
-    async_session = async_sessionmaker(engine, class_=AsyncSession)
-    async with async_session() as session:
-        async with session.begin():
-            session.add(AsyncAttrs())
-        await session.flush()
-        await session.commit()
-        await session.close()
+    async_session = sessionmaker(engine, class_=AsyncSession)
+    Base.metadata.create_all(engine.sync_engine)
+    async with async_session() as sess:
+        async with sess.begin():
+            sess.add(A(data="a1"))
+        await sess.flush()
+        await sess.commit()
+        await sess.close()
+
+        res = (await sess.execute(select(A))).scalars().one()
+        assert res.data == "a1"
