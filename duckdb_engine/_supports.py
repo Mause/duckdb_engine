c54163ca7eafa855d165867f6ed2fe090a9d793f
Elliana May <me@mause.me>
2024-02-14 15:44:42 +0800

    feat: preinstall extensions

--- duckdb_engine/_supports.py
+++ duckdb_engine/_supports.py
@@ -1,11 +1,28 @@
+from typing import Callable
+
 import duckdb
 from packaging.version import Version
 
 duckdb_version = Version(duckdb.__version__)  # type: ignore[attr-defined]
 supports_attach = duckdb_version >= Version("0.7.0")
-supports_user_agent = duckdb_version >= Version("0.9.2")
+has_extension_registry_support = supports_user_agent = duckdb_version >= Version(
+    "0.9.2"
+)
 
 
+def cache(func: Callable[[], bool]) -> Callable[[], bool]:
+    value = None
+
+    def wrapper() -> bool:
+        nonlocal value
+        if value is None:
+            value = func()
+        return value
+
+    return wrapper
+
+
+@cache
 def has_comment_support() -> bool:
     """
     See https://github.com/duckdb/duckdb/pull/10372
@@ -17,3 +34,9 @@ def has_comment_support() -> bool:
     except duckdb.ParserException:
         return False
     return True
+
+
+"""
+See https://github.com/duckdb/duckdb/pull/10437
+"""
+has_extension_version_support = False
