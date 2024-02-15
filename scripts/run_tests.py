492a1cad9284165714f9b0ce2c3c53ef4365d3fa
Elliana May <me@mause.me>
2024-02-15 23:39:48 +0800

    try to fix nightly

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -24,16 +24,26 @@ def main() -> None:
     )
 
 
+def install(*args: str) -> None:
+    check_call([sys.executable, "-m", "pip", "install", *args])
+
+
 def install_duckdb() -> None:
     tox_env_name = os.environ.get("TOX_ENV_NAME")
-    if not tox_env_name or "-" not in tox_env_name:
+
+    if not tox_env_name:
         return
 
-    duckdb_version = ".".join(tox_env_name.split("-")[1][len("duckdb") :])
+    elif tox_env_name == "duckdb_master":
+        print("Installing DuckDB from master for testing")
+        install("duckdb", "--pre")
 
-    print(f"Installing DuckDB version {duckdb_version} for testing")
+    else:
+        duckdb_version = ".".join(tox_env_name.split("-")[1][len("duckdb") :])
 
-    check_call([sys.executable, "-m", "pip", "install", f"duckdb=={duckdb_version}"])
+        print(f"Installing DuckDB version {duckdb_version} for testing")
+
+        install(f"duckdb=={duckdb_version}")
 
 
 if __name__ == "__main__":
