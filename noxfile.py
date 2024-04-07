e104eb9b9885db1d8e84ffbd9e361f172a8898ba
Elliana May <me@mause.me>
2024-04-07 23:49:04 +0800

    build: reduce combinations

--- noxfile.py
+++ noxfile.py
@@ -33,12 +33,19 @@ envlist = expandvars(
 )
 
 
-@nox.session(py=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
-@nox.parametrize(
-    "duckdb", ["0.5.1", "0.6.1", "0.7.1", "0.8.1", "0.9.1", "0.10.1", "master"]
-)
+@nox.session(py=["3.7", "3.8", "3.9", "3.10"])
+@nox.parametrize("duckdb", ["0.5.1", "0.6.1", "0.7.1", "0.8.1", "0.9.1", "0.10.1"])
 @nox.parametrize("sqlalchemy", ["1.3", "1.4", "2.0"])
 def tests(session: nox.Session, duckdb: str, sqlalchemy: str) -> None:
+    tests_core(session, duckdb, sqlalchemy)
+
+
+@nox.session(py=["3.8"])
+def nightly(session: nox.Session) -> None:
+    tests_core(session, "master", "1.4")
+
+
+def tests_core(session: nox.Session, duckdb: str, sqlalchemy: str) -> None:
     with group(f"{session.name} - Install"):
         poetry(session)
         session.install(f"sqlalchemy~={sqlalchemy}")
@@ -64,7 +71,7 @@ def poetry(session: nox.Session) -> None:
     session.run("poetry", "install", "--with", "dev")
 
 
-@nox.session()
+@nox.session(py=["3.8"])
 def mypy(session: nox.Session) -> None:
     session.run("mypy", ".")
 
