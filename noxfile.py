7851a6c056c826b26bf5b4ae94d948cc38523d45
Elliana May <me@mause.me>
2024-04-07 21:08:52 +0800

    build: add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,58 @@
+import re
+from itertools import product
+from typing import List
+
+import nox
+
+
+def expandvars(string: str) -> List[str]:
+    bits = [
+        part[1:-1].split(",") if part.startswith("{") else [part]
+        for part in (re.split(r"(\{[^}]+})", string))
+    ]
+    return ["".join(s) for s in product(*bits)]
+
+
+envlist = expandvars(
+    "{mypy,duckdb040,duckdb051,duckdb061,duckdb071,duckdb081}-sqlalchemy{13,14,20}"
+)
+
+
+@nox.session(py=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.parametrize(
+    "duckdb", ["0.4.0", "0.5.1", "0.6.1", "0.7.1", "0.8.1", "0.9.1", "0.10.1", "master"]
+)
+@nox.parametrize("sqlalchemy", ["1.3", "1.4", "2.0"])
+def tests(session: nox.Session, duckdb: str, sqlalchemy: str) -> None:
+    poetry(session)
+    session.install(f"sqlalchemy~={sqlalchemy}")
+    if duckdb == "master":
+        session.install("duckdb", "--pre", "-U")
+    else:
+        session.install(f"duckdb=={duckdb}")
+    session.run(
+        "pytest",
+        "--junitxml=results.xml",
+        "--cov",
+        "--cov-report",
+        "xml:coverage.xml",
+        "--verbose",
+        "-rs",
+        "--remote-data",
+    )
+
+
+def poetry(session: nox.Session) -> None:
+    session.install("poetry")
+    session.run("poetry", "install", "--with", "dev")
+
+
+@nox.session()
+def mypy(session: nox.Session) -> None:
+    session.run("mypy", ".")
+
+
+# passenv = HOME
+# setenv =
+#     SQLALCHEMY_WARN_20: true
+# envlogdir = logs
