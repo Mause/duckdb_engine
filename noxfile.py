771b8cc865f02329e3397af7ebb3fc5cb3595011
Elliana May <me@mause.me>
2024-04-08 02:06:15 +0800

    build: install mypy using poetry

--- noxfile.py
+++ noxfile.py
@@ -59,7 +59,7 @@ def poetry(session: nox.Session) -> None:
 
 @nox.session(py=["3.8"])
 def mypy(session: nox.Session) -> None:
-    session.install("mypy")
+    poetry(session)
     session.run("mypy", ".")
 
 
